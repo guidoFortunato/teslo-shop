bbc132c89fd8ad17698811fdce7b1189f0b9b39e
guido fortunato <guido_fortunato@hotmail.com.ar>
2024-03-27 18:56:06 -0300

    selected sizes and quantity

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import { titleFont } from "@/config/fonts";
-import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+import { titleFont } from "@/config/fonts";
+import { QuantitySelector, SizeSelector } from "@/components";
+import { initialData } from "@/seed/seed";
 
 interface Props {
   params: {
@@ -26,28 +27,24 @@ export default function ProductPage({ params }: Props) {
         <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
           {product.title}
         </h1>
-        <p className="text-lg mb-5">${ product.price }</p>
+        <p className="text-lg mb-5">${product.price}</p>
 
         {/* Selector de tallas */}
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
 
         {/* Selector de cantidad */}
-
+        <QuantitySelector quantity={2} />
 
         {/* Boton */}
-        <button className="btn-primary my-5">
-          Agregar al carrito
-        </button>
+        <button className="btn-primary my-5">Agregar al carrito</button>
 
         {/* Descripción */}
         <h3 className="font-bold text-sm">Descripción</h3>
 
-        <p className="font-light">
-          { product.description }
-        </p>
-
-
-
-
+        <p className="font-light">{product.description}</p>
       </div>
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 
+export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
+
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
