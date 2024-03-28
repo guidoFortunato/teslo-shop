7d367051ee82d42a663178d1bbfd86777feb4c1c
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-03-28 16:34:03 -0300

    swiper

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { titleFont } from "@/config/fonts";
-import { QuantitySelector, SizeSelector } from "@/components";
+import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { initialData } from "@/seed/seed";
 
 interface Props {
@@ -20,7 +20,9 @@ export default function ProductPage({ params }: Props) {
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
-      <div className="col-span-1 md:col-span-2">hola</div>
+      <div className="col-span-1 md:col-span-2">
+        <ProductSlideshow title={product.title} images={product.images} />
+      </div>
 
       {/* Detalles */}
       <div className="col-span-1 px-5">
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./products/product-grid/ProductGridItem";
 
 export * from "./product/size-selector/SizeSelector";
 export * from "./product/quantity-selector/QuantitySelector";
+export * from "./product/slideshow/ProductSlideshow";
 
 export * from "./ui/sidebar/Sidebar";
 export * from "./ui/not-found/PageNotFound";
