df4d3b4533bf4d1ffe469b6767cdbcf1dcac8e58
guido fortunato <guido_fortunato@hotmail.com.ar>
2024-04-05 15:22:24 -0300

    orders

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -22,29 +22,52 @@ export default function CartPage() {
             <Link href="/" className="underline mb-5">
               Continúa comprando
             </Link>
+
+            {/* Items */}
+
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px"
+                  }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
           </div>
 
-          {/* Items */}
+          {/* Checkout - Resumen de orden */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2 ">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
 
-          {productsInCart.map((product) => (
-            <div key={product.slug} className="flex">
-              <Image
-                src={`/products/${product.images[0]}`}
-                width={100}
-                height={100}
-                alt={ product.title }
-                className="mr-5 rounded"
-              />
-              <div>
-                <p>{ product.title }</p>
-                <p>${ product.price }</p>
-                <QuantitySelector quantity={ 3 } />
-                <button className="underline mt-3">Remover</button>
-              </div>
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-right">$ 100</span>
             </div>
-          ))}
-
-          {/* Checkout */}
+            <div className="mt-5 mb-2 w-full">
+              <Link className="flex btn-primary justify-center" href="/checkout/address">Checkout</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
